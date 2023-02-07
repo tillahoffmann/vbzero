7c044844252db13f4e28eb6343247e26d46896ae
Till Hoffmann <tillahoffmann@gmail.com>
2023-02-07 12:45:25 -0500

    Cache myst notebook execution.

--- conf.py
+++ conf.py
@@ -1,3 +1,6 @@
+from sphinx.application import Sphinx
+
+
 master_doc = "README"
 extensions = [
     "myst_nb",
@@ -15,6 +18,10 @@ plot_formats = [
 ]
 html_theme = "sphinx_rtd_theme"
 
+nb_execution_mode = "cache"
+nb_execution_raise_on_error = True
+nb_execution_timeout = 60
+
 # Configure autodoc to avoid excessively long fully-qualified names.
 add_module_names = False
 autodoc_typehints_format = "short"
@@ -24,3 +31,8 @@ intersphinx_mapping = {
     "numpy": ("https://numpy.org/doc/stable/", None),
     "torch": ("https://pytorch.org/docs/stable/", None),
 }
+
+
+def setup(sphinx: Sphinx) -> None:
+    # Prevent execution of jupyter notebooks.
+    sphinx.registry.source_suffix.pop(".ipynb", None)
