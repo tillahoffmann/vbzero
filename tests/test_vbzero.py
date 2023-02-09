ede68fe32d97fa8fe9d554ef5a88ed6c0db922c6
Till Hoffmann <tillahoffmann@gmail.com>
2023-02-09 17:57:48 -0500

    Add extended conditioning and gradient test.

--- tests/test_vbzero.py
+++ tests/test_vbzero.py
@@ -34,3 +34,55 @@ def test_integration() -> None:
     actual = distributions()["proba"].mean
 
     assert (expected - actual).abs() < 0.1
+
+
+def test_prior_and_conditioned_model() -> None:
+    @vz.util.model(return_state=True)
+    def model():
+        # Hyperparameters.
+        n = 50
+        sigma = 1
+        length_scale = 0.1
+        kappa = 0.1
+        # Sample from the model.
+        x = vz.util.sample("x", th.distributions.Normal(0, 1), sample_shape=n)
+        cov = sigma * (- (x[:, None] - x) ** 2 / (2 * length_scale ** 2)).exp() + 1e-3 * th.eye(n)
+        w = vz.util.sample("w", th.distributions.Normal(0, 1), sample_shape=n)
+        z = vz.util.record("z", th.linalg.cholesky(cov) @ w)
+        vz.util.sample("y", th.distributions.Normal(z, kappa))
+
+    # Check that conditioning works here.
+    state = model()
+    assert set(state) == set("wxyz")
+    conditioned = vz.util.condition(model, w=state["w"], x=state["x"])
+    prediction = conditioned()
+    th.testing.assert_close(state["w"], prediction["w"])
+    th.testing.assert_close(state["x"], prediction["x"])
+    th.testing.assert_close(state["z"], prediction["z"])
+
+    # Evaluate an estimate of the log likelihood.
+    n, = state["x"].size()
+    approximation = vz.nn.ParameterizedDistributionDict({
+        "w": vz.nn.ParameterizedDistribution(th.distributions.Normal, loc=th.randn(n),
+                                             scale=th.ones(n)),
+    })
+    dist: vz.nn.DistributionDict = approximation()
+    sample = dist.rsample()
+    with vz.util.LogProb() as log_prob:
+        vz.util.condition(model, state["x", "y"], sample)()
+
+    # Check that gradients exist.
+    loss: th.Tensor = vz.util.maybe_aggregate(log_prob, True)
+    loss.backward()
+    for parameter in approximation.parameters():
+        # Check gradients exist for all parameters and reset them.
+        assert parameter.grad is not None
+        parameter.grad = None
+
+    # Check that gradients exist for the entropy.
+    dist: vz.nn.DistributionDict = approximation()
+    loss = dist.entropy(aggregate=True)
+    loss.backward()
+    # Location does not contribute to the entropy so should have no gradient.
+    assert approximation["w"].unconstrained.loc.grad is None
+    assert approximation["w"].unconstrained.scale.grad is not None
